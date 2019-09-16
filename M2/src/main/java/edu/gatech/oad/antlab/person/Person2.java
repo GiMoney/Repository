2bee3127a8dcfeff41ad7e6b5d8dc40c81811c0a
druzinov <druzy615@gmail.com>
2019-09-16 00:29:33 -0400

    Updated Person2 with completed calc method

--- M2/src/main/java/edu/gatech/oad/antlab/person/Person2.java
+++ M2/src/main/java/edu/gatech/oad/antlab/person/Person2.java
@@ -1,47 +1,61 @@
-package edu.gatech.oad.antlab.person;
+package main.java.edu.gatech.oad.antlab.person;
 
 /**
  *  A simple class for person 2
  *  returns their name and a
  *  modified string 
  *
- * @author Bob
+ * @author Daniella Ruzinov
  * @version 1.1
  */
 public class Person2 {
+
     /** Holds the persons real name */
     private String name;
-	 	/**
-	 * The constructor, takes in the persons
-	 * name
-	 * @param pname the person's real name
-	 */
-	 public Person2(String pname) {
-	   name = pname;
-	 }
-	/**
-	 * This method should take the string
-	 * input and return its characters in
-	 * random order.
-	 * given "gtg123b" it should return
-	 * something like "g3tb1g2".
-	 *
-	 * @param input the string to be modified
-	 * @return the modified string
-	 */
-	private String calc(String input) {
-	  //Person 2 put your implementation here
-	  return null;
-	}
-	/**
-	 * Return a string rep of this object
-	 * that varies with an input string
-	 *
-	 * @param input the varying string
-	 * @return the string representing the 
-	 *         object
-	 */
-	public String toString(String input) {
-	  return name + calc(input);
-	}
+
+    /**
+     * The constructor, takes in the persons
+     * name
+     * @param pname the person's real name
+     */
+    public Person2(String pname) {
+        name = pname;
+    }
+
+    /**
+     * This method should take the string
+     * input and return its characters in
+     * random order.
+     * given "gtg123b" it should return
+     * something like "g3tb1g2".
+     *
+     * @param input the string to be modified
+     * @return the modified string
+     */
+    private String calc(String input) {
+        if (input.equals("")) {
+            return "";
+        }
+        String jumbled = "";
+        int randInd = (int) (input.length() * Math.random());
+
+        for (int i = 0; i < input.length(); i++) {
+            jumbled += input.charAt(randInd);
+            input = input.substring(0, randInd) + input.substring(randInd + 1);
+            randInd = (int) (input.length() * Math.random());
+        }
+        return jumbled;
+    }
+
+    /**
+     * Return a string rep of this object
+     * that varies with an input string
+     *
+     * @param input the varying string
+     * @return the string representing the
+     *         object
+     */
+    public String toString(String input) {
+        return name + calc(input);
+    }
 }
