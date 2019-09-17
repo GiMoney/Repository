8a60519f476daa2d5fc87865d2b9e28f5162ce68
Lincoln Anders <lincolnanders5@gmail.com>
2019-09-16 20:39:09 -0400

    modified method

--- M2/src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
+++ M2/src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
@@ -69,16 +69,14 @@ import edu.gatech.oad.antlab.pkg2.*;
 		  toPrint += p3.toString("gburdell3");
                   //Person4 replace P4 with your name
                   //and gburdell4 with your gt id
-                  Person4 p4 = new Person4("P4");
-                  toPrint += p4.toString("gburdell4");
+                  Person4 p4 = new Person4("Lincoln Anders");
+                  toPrint += p4.toString("landers8");
                   //Person5 replace P4 with your name
                   //and gburdell5 with your gt id
                   Person5 p5 = new Person5("P5");
                   toPrint += p5.toString("gburdell5");
 		  
         System.out.println(toPrint);
-        
-        
     }
      
     
@@ -90,8 +88,4 @@ import edu.gatech.oad.antlab.pkg2.*;
         new AntLabMain().printOutMessage();
         
      } 
-    
-    
-    
-    
- } 
+} 
